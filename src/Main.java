b37b0de4b0be7b02094f2437ae6da6b4af721c31
79773 <ussok17@mail.ru>
2022-09-23 21:57:15 +0300

    Сделано ДЗ

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+var frends = 19;
+        System.out.println(frends);
+        frends = frends + 2;
+        System.out.println(frends);
+        frends = frends / 7;
+        System.out.println(frends);
+        // Задача 2
+        System.out.println("Задача 2");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 10;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+    }
+}
